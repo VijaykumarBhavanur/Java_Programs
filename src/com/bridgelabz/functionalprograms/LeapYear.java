4badc3efc257c8d5d071c4c443e1ecbac8b10296
Vijaykumar Bhavanur <vijaykumarbhavanur@gmail.com>
2019-08-16 17:05:46 +0530

    Basic Logical Programs

--- src/com/bridgelabz/functionalprograms/LeapYear.java
+++ src/com/bridgelabz/functionalprograms/LeapYear.java
@@ -10,16 +10,14 @@ public class LeapYear {
 		CommonUtility util=new CommonUtility();
 		System.out.println("Enter an year to check whether it is Leap: ");
 		int year=scanner.nextInt();
-		
 		if(util.getDigitCount(year)<4)
 			System.out.println("Invalid Input year should have  4 digits");
 		else
 		{
-			if(year%400==0 || year%4==0&&year%100!=0)
+			if(year%400==0 || year%4==0 && year%100!=0)
 				System.out.println(year+" is leap year");
 			else
 				System.out.println(year+" is not leap year");
-			
 		}
 	}
 }
