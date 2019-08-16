8b7eef8f7505b50b77ae50ebfbfdc321a049818e
Vijaykumar Bhavanur <vijaykumarbhavanur@gmail.com>
2019-08-16 18:16:52 +0530

    Your commit message

---
+++ src/com/bridgelabz/functionalprograms/StopWatch.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.functionalprograms;
+
+public class StopWatch {
+
+	public static void main(String[] args) {
+	    int a[]= {-2,3,-1,5,4,-9,8,-6,11,-3,1,7,14,45,-15,-30};
+	    int tripletCount=0;
+	    
+	    for (int i = 0; i < a.length; i++) {
+			for (int j = i+1; j < a.length; j++) {
+				for (int k = j+1; k < a.length; k++) {
+					if(a[i]+a[j]+a[k]==0)
+					{
+						System.out.println("["+a[i]+","+a[j]+","+a[k]+"]");
+						tripletCount++;
+					}
+				}
+			}
+		}
+	    System.out.println("Number of Triplets whose value is Zero: "+tripletCount);
+	}
+
+}
