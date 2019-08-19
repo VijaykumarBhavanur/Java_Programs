62697a57c164e84dc65e1b9d1e2b0e110f57f512
admin1 <admin1@admin1-LIVA-Z2V>
2019-08-19 18:33:08 +0530

    1) Stop-Watch Program implementation.
    2) Tested whether row-wise,diagonal-wise elements are same for tic-toe
    program.

---
+++ src/com/bridgelabz/functionalprograms/StopWatch.java
@@ -0,0 +1,69 @@
+package com.bridgelabz.functionalprograms;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class StopWatch 
+{
+	public static void main(String[] args) 
+	{			
+		LocalDateTime time=LocalDateTime.now();
+		Scanner scanner=new Scanner(System.in);
+		
+		int beforeStartHour=0;
+		int beforeStartMinutes=0;
+		int beforeStartSeconds=0;
+		
+		int afterStopHour=0;
+		int afterStopMinutes=0;
+		int afterStopSeconds=0;
+		
+		while(true)
+		{
+			System.out.println("Press S to start the watch: ");
+			char c=scanner.next().charAt(0);
+		
+			if(c!='s'&&c!='S')
+				System.out.println("Invalid input: ");
+			else
+				break;
+		}
+		
+		beforeStartHour=time.getHour();
+		beforeStartMinutes=time.getMinute();
+		beforeStartSeconds=time.getSecond();
+		
+		System.out.println("********************Begin time of Stop-Watch****************");
+		System.out.println(beforeStartHour+"hr :"+beforeStartMinutes+"min :"+beforeStartSeconds+"sec");
+		
+		int beforeTime=beforeStartHour*3600+beforeStartMinutes*60+beforeStartSeconds;
+		
+		while(true)
+		{
+			System.out.println("Press Q to stop the watch: ");
+			char c=scanner.next().charAt(0);
+			if(c!='q'&&c!='Q')
+				System.out.println("Invalid input: ");
+			else
+				break;
+		}
+		
+		LocalDateTime time1=LocalDateTime.now();
+		afterStopHour=time1.getHour();
+		afterStopMinutes=time1.getMinute();
+		afterStopSeconds=time1.getSecond();
+		
+		System.out.println("********************Stop time of Stop-Watch****************");
+		System.out.println(afterStopHour+"hr :"+afterStopMinutes+"min :"+afterStopSeconds+"sec");
+		
+		int aftertime=afterStopHour*3600+afterStopMinutes*60+afterStopSeconds;
+		
+		int r1=(aftertime-beforeTime)%3600;
+		int totalhr=(aftertime-beforeTime)/3600;
+		int r2=r1%60;
+		int totlamin=r1/60;
+		int totalsec=r2;
+		
+		System.out.println("***************Total time elapsed*************************");
+		System.out.println(totalhr+"hr :"+totlamin+"min :"+totalsec+"sec");
+	}
+}
--- src/com/bridgelabz/functionalprograms/TestClass.java
+++ src/com/bridgelabz/functionalprograms/TestClass.java
@@ -1,31 +1,86 @@
 package com.bridgelabz.functionalprograms;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class TestClass {
 
 	public static void main(String[] args) 
 	{
-		String str="gadag";
-		char[] c=str.toCharArray();
-		LinkedList ll=new LinkedList();
+		/*
+		 * int h1=4; int m1=4; int s1=35;
+		 * 
+		 * int c1=h1*3600+m1*60+s1; //System.out.println(c1);
+		 * 
+		 * int h2=4; int m2=16; int s2=8;
+		 * 
+		 * int c2=h2*3600+m2*60+s2; //System.out.println(c2);
+		 * 
+		 * //System.out.println(c2-c1);
+		 * 
+		 * int r1=(c2-c1)%3600; int hr=(c2-c1)/3600; int r2=r1%60; int min=r1/60; int
+		 * sec=r2;
+		 * 
+		 * System.out.println(hr+"hr:"+min+"min:"+sec+"sec");
+		 */
 		
-		for (int i = 0; i < c.length; i++)
-			ll.add(c[i]);
-
-	int index=ll.size()-1;
-	for (int i = 0; i <ll.size()/2; i++) 
-	{
-			if(ll.get(i)!=ll.get(index))
-			{
-				System.out.println("Not palindrome");
-				return;
-			}
+		
+		char array[][]= {{'1','0','1'},{'0','0','1'},{'1','0','1'}};
+		
+		int row=0;
+		int d1=0,d2=0;
+		
+		for (int i = 0; i < array.length; i++) 
+		{
+				for (int j = 0; j < array.length; j++)
+				{
+						if(array[i][j]=='1')
+							row++;
+						else
+							row=0;
+						 if(i==j&&array[i][j]=='1')
+							d1++;
+						if(i+j==2&&array[i][j]=='1')
+							d2++;
+				}
 			
-				index--;
-	}
-
-	System.out.println("Palindrome");
-	}
-
+			if(row==3)
+				System.out.println("row has all 1's ");	
+		}
+		
+			if(d1==3)
+				System.out.println("diagonal1 has only 1's ");
+			else if(d2==3)
+				System.out.println("diagonal2 has only 1's ");
+			
+			else
+				System.out.println("all are different!!!");
+			
+			
+			
+		/*for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				 if(i==j&&array[i][j]=='1')
+					d1++;
+			}
+		}
+		if(d1==3)
+			System.out.println("diagonal1 has only 1's ");
+		else
+			System.out.println("diagonal has different elements");
+		
+		
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				 if(i+j==2&&array[i][j]=='1')
+					d2++;
+			}
+		}
+		if(d2==3)
+			System.out.println("diagonal2 has only 1's ");
+		else
+			System.out.println("diagonal has different elements");
+		}	*/
+ }
 }
